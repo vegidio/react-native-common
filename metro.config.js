2c0376d4e700092189549a7aa0d6e3598c1d7749
Vinicius Egidio <me@vinicius.io>
2023-03-06 10:35:03 +0100

    🐛 Changed Metro config to load ESM modules with exports

--- metro.config.js
+++ metro.config.js
@@ -14,4 +14,18 @@ module.exports = {
             },
         }),
     },
+    resolver: {
+        // While the newest version of Metro, that will support exports, doesn't come:
+        // https://github.com/facebook/metro/issues/670
+        resolveRequest: (context, moduleName, platform) => {
+            if (moduleName.startsWith('graphql-request')) {
+                return {
+                    filePath: `${__dirname}/node_modules/graphql-request/build/esm/index.js`,
+                    type: 'sourceFile',
+                };
+            }
+
+            return context.resolveRequest(context, moduleName, platform);
+        },
+    },
 };
